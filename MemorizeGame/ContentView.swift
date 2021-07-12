46e8c08a9f613acac499043142f5db94a7f1addb
Hongbin Mao <hello2mao@gmail.com>
2021-07-12 08:08:28 +0800

    Lazy push: 2021年 7月12日 星期一 08时08分28秒 CST

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🌵", "🦨", "🍄", "🐶", "🐸", "🐝", "🪰", "🐖", "🐛", "🍩", "🍓", "🐞", "🐬", "🐯", "🌝", "💍", "👻", "🩴", "😎", "👜", "🐁"]
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                Text("得分：")
+                Text("100")
+            }
+            .foregroundColor(Color.orange)
+            .font(.largeTitle)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+                    ForEach(emojis[0..<20], id: \.self) {
+                        emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    }
+                }
+            }
             .padding()
+            .foregroundColor(.orange)
+        }
+
     }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 10)
+            if isFaceUp {
+                shape.fill(Color.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill(Color.orange)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
